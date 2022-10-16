56b9284c26a0d6f82fc9f343907d6e2a720fd417
pankaj10032 <97507747+pankaj10032@users.noreply.github.com>
2022-10-16 16:30:23 +0530

    Add files via upload
    Here we are detecting all the the letters from A to Z by using American sign language in which we are  Representing all letters one by one by some configuration of our hand.
    In real life we can use it by opening some locker or any gate or any another. Here First we gave a configuration of our hand many times (almost 200-300) to the camera present in the locker.
    Then further we configure our hand in that position, so that we can open the lock again.
    # Here I am uploading some testing images(on which i have trained my model) and also i am uploading some of the images that i got during testing with some of the labels from A to Z.

---
+++ Test.py
@@ -0,0 +1,217 @@
+import mediapipe as mp
+import cv2
+import numpy as np
+import time
+from time import sleep
+import math
+from cvzone.ClassificationModule import Classifier
+# from pynput.keyboard import Controller
+class HandDetector:
+
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, minTrackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.minTrackCon = minTrackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode, max_num_hands=self.maxHands,
+                                        min_detection_confidence=self.detectionCon,
+                                        min_tracking_confidence=self.minTrackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
+        self.fingers = []
+        self.lmList = []
+
+    def findHands(self, img, draw=True, flipType=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        allHands = []
+        h, w, c = img.shape
+        if self.results.multi_hand_landmarks:
+            for handType, handLms in zip(self.results.multi_handedness, self.results.multi_hand_landmarks):
+                myHand = {}
+                ## lmList
+                mylmList = []
+                xList = []
+                yList = []
+                for id, lm in enumerate(handLms.landmark):
+                    px, py, pz = int(lm.x * w), int(lm.y * h), int(lm.z * w)
+                    mylmList.append([px, py, pz])
+                    xList.append(px)
+                    yList.append(py)
+
+                ## bbox
+                xmin, xmax = min(xList), max(xList)
+                ymin, ymax = min(yList), max(yList)
+                boxW, boxH = xmax - xmin, ymax - ymin
+                bbox = xmin, ymin, boxW, boxH
+                cx, cy = bbox[0] + (bbox[2] // 2), \
+                         bbox[1] + (bbox[3] // 2)
+
+                myHand["lmList"] = mylmList
+                myHand["bbox"] = bbox
+                myHand["center"] = (cx, cy)
+
+                if flipType:
+                    if handType.classification[0].label == "Right":
+                        myHand["type"] = "Left"
+                    else:
+                        myHand["type"] = "Right"
+                else:
+                    myHand["type"] = handType.classification[0].label
+                allHands.append(myHand)
+
+                ## draw
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+                #mpDraw.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+                #mpDraw.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2
+                    cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
+                                  (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),
+                                  (255, 0, 255), 2)
+                    cv2.putText(img, myHand["type"], (bbox[0] - 30, bbox[1] - 30), cv2.FONT_HERSHEY_PLAIN,
+                                2, (255, 0, 255), 2)
+        if draw:
+            return allHands, img
+        else:
+            return allHands
+
+    def fingersUp(self, myHand):
+  # It will find which fingers are open and it will return number of fingers which are up.
+        myHandType = myHand["type"]
+        myLmList = myHand["lmList"]
+        if self.results.multi_hand_landmarks:
+            fingers = []
+            # Thumb
+            if myHandType == "Right":
+                if myLmList[self.tipIds[0]][0] > myLmList[self.tipIds[0] - 1][0]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+            else:
+                if myLmList[self.tipIds[0]][0] < myLmList[self.tipIds[0] - 1][0]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+
+            # 4 Fingers
+            for id in range(1, 5):
+                if myLmList[self.tipIds[id]][1] < myLmList[self.tipIds[id] - 2][1]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+        return fingers
+
+    def findDistance(self, p1, p2, img=None):
+        x1, y1 = p1
+        x2, y2 = p2
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        length = math.hypot(x2 - x1, y2 - y1)
+        info = (x1, y1, x2, y2, cx, cy)
+        if img is not None:
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            return length, info, img
+        else:
+            return length, info
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector(detectionCon=0.8, maxHands=2)
+    counter = 0
+    folder = "Data/C"
+    imgSize = 300
+    offset = 20
+    labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
+             "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+              "W", "X", "Y", "Z"]
+    classifier = Classifier("model/keras_model.h5", "model/labels.txt")
+
+    finaltext=" "
+    #keyboard=Controller()
+    while True:
+        # Get image frame
+        success, img = cap.read()
+        imgOutput=img.copy()
+        # Find the hand and its landmarks
+        hands, img = detector.findHands(img)  # with draw
+        # hands = detector.findHands(img, draw=False)  # without draw
+
+        if hands:
+            # Hand 1
+            hand1 = hands[0]
+            lmList1 = hand1["lmList"]  # List of 21 Landmark points
+            x,y,w,h = hand1["bbox"]  # Bounding box info x,y,w,h
+            centerPoint1 = hand1['center']  # center of the hand cx,cy
+            handType1 = hand1["type"]  # Handtype Left or Right
+
+            fingers1 = detector.fingersUp(hand1)
+
+            imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255  # 0-> black pixels and 255-> white pixels
+            imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+            # # it is juso a matrix(staring-ending height and starting_ending width)
+
+            imgCropShape = imgCrop.shape
+            # imgWhite[o:imgCropShape[0],0:imgCropShape[1]]=imgcrop
+            aspectRatio = h / w
+            if aspectRatio > 1:
+                k = imgSize / h
+                wCal = math.ceil(k * w)  # always goes to above Integar value
+                imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+                imgResizeShape = imgResize.shape
+                wGap = math.ceil((imgSize - wCal) / 2)  # here width of our white image is 300
+                imgWhite[:, wGap:wCal + wGap] = imgResize  # because width of imgResize should be Wcal.
+                prediction, index = classifier.getPrediction(imgWhite, draw=False)
+                print(prediction, index)
+
+            else:
+                k = imgSize / w
+                hCal = math.ceil(k * h)
+                imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+                imgResizeShape = imgResize.shape
+                hGap = math.ceil((imgSize - hCal) / 2)
+                imgWhite[hGap:hCal + hGap, :] = imgResize  # Here height of image Resise should e hCal.
+                prediction, index = classifier.getPrediction(imgWhite, draw=False)
+                print(prediction, index)
+            if len(hands) == 2:
+                # Hand 2
+                hand2 = hands[1]
+                lmList2 = hand2["lmList"]  # List of 21 Landmark points
+                bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
+                centerPoint2 = hand2['center']  # center of the hand cx,cy
+                handType2 = hand2["type"]  # Hand Type "Left" or "Right"
+
+                fingers2 = detector.fingersUp(hand2)
+
+                # Find Distance between two Landmarks. Could be same hand or different hands
+                length, info, img = detector.findDistance(lmList1[8][0:2], lmList2[8][0:2], img)  # with draw
+                # length, info = detector.findDistance(lmList1[8], lmList2[8])  # with draw
+            cv2.rectangle(imgOutput, (x - offset, y - offset - 50),
+                          (x - offset + 90, y - offset - 50 + 50), (255, 0, 255), cv2.FILLED)
+            cv2.putText(imgOutput, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+            #keyboard.press(labels[index])
+            finaltext+=labels[index]
+            cv2.rectangle(imgOutput, (x - offset, y - offset),
+                          (x + w + offset, y + h + offset), (255, 0, 255), 4)
+            cv2.rectangle(imgOutput,(20,350),(700,450),(180,180,2400),-1)
+            cv2.putText(imgOutput,finaltext,(0,400),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,255),5)
+            sleep(0.2)
+            #cv2.putText(imgOutput, str(prediction), (20, 40), cv2.FONT_HERSHEY_COMPLEX, 3, (0, 255, 0), 1)
+        # Display
+            cv2.imshow("imagecrop",imgCrop)
+            cv2.imshow("imagewhite",imgWhite)
+        cv2.imshow("output imahe",imgOutput)
+        key=cv2.waitKey(1)
+        if key == ord("s"):
+            counter += 1
+            cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)  # To save the image.
+            # Here time.time() will give a unique name to every image that we have capture
+            print(counter)  # How  many images did we have saved.
+
+if __name__ == "__main__":
+    main()
